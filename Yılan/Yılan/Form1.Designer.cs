3ba2185713db7811a53a151b24e88af090a299c4
semihceltik <56488622+semihceltik@users.noreply.github.com>
2021-01-08 11:43:00 +0300

    Semih
    SnakeGame c# form

---
+++ Yılan/Yılan/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace Yılan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.game = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.kontrol = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.game)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // game
+            // 
+            this.game.BackColor = System.Drawing.SystemColors.Info;
+            this.game.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.game.Location = new System.Drawing.Point(31, 44);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(500, 496);
+            this.game.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.game.TabIndex = 0;
+            this.game.TabStop = false;
+            this.game.Click += new System.EventHandler(this.game_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Süre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Skor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Süre";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Skor";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textbox);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(123, 275);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 99);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "İSİM";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(206, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 67);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // textbox
+            // 
+            this.textbox.Location = new System.Drawing.Point(43, 63);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(157, 22);
+            this.textbox.TabIndex = 4;
+            this.textbox.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            this.textbox.Enter += new System.EventHandler(this.textbox_Enter);
+            this.textbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbox_KeyPress);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(93, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(50, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "zor";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "kolay";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "OYUNU BAŞLATMAK İÇİN B ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(311, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "OYUNU DURDURMAK İÇİN D TUŞUNA BASINIZ";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Location = new System.Drawing.Point(123, 399);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(321, 128);
+            this.panel2.TabIndex = 7;
+            this.panel2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(6, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 49);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "YENİDEN BAŞLA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(14, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(299, 55);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "KAYBETTİN";
+            // 
+            // kontrol
+            // 
+            this.kontrol.AutoSize = true;
+            this.kontrol.Location = new System.Drawing.Point(392, 207);
+            this.kontrol.Name = "kontrol";
+            this.kontrol.Size = new System.Drawing.Size(46, 17);
+            this.kontrol.TabIndex = 8;
+            this.kontrol.Text = "label9";
+            this.kontrol.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(211, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "YENİDEN BAŞLA";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(587, 622);
+            this.Controls.Add(this.kontrol);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.game);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "YılanOyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.game)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox game;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label kontrol;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Yılan/Yılan/Form1.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace Yılan
+{
+    public partial class Form1 : Form
+    {
+        #region DEĞİŞKENLER
+        
+        public int hız = 200;
+        int skor = 0;
+        int süre = 0;
+        int süre2 = 1;
+        public  bool durdurma;
+        public bool oyun = true;
+
+
+       
+        #endregion
+        #region YÖNBİLGİLERİ
+
+        public enum direction
+        {
+
+            up,
+            down,
+            right,
+            left
+        }
+        public direction yön = direction.right;
+        public int x = 1, y = 1;
+
+        #endregion
+        #region KUYRUK OLUŞTURMA
+        public List<Konum> kuyruk = new List<Konum>();
+        #endregion
+
+        #region KONUMYARATMA
+        public class Konum
+        {
+            
+            
+            public int xxx = 0;
+            public int yyy = 0;
+
+            public Konum(int xs, int ys)
+            {
+                
+                this.xxx = xs;
+                this.yyy = ys;
+            }
+        }
+        public Konum yemek = new Konum(-1, -1); // Yemek oluşturmak için kullanılan konum
+
+        #endregion
+        #region FORM1
+        public Form1()
+        {
+            InitializeComponent();
+            #region KUYRUK EKLEME
+            kuyruk.Add(new Form1.Konum(0, 0));
+            #endregion
+        }
+        #endregion
+        #region HAREKET ETME
+        public void oyunubaslat()
+        {
+
+
+            Thread thread = new Thread(new ThreadStart(new Action(() =>
+            {
+                while (oyun) // SINIRSIZ DONGÜ
+                {
+                    if (yön == direction.right)
+                    {
+                        x++;
+                    }
+                    if (yön == direction.left)
+                    {
+                        x--;
+                    }
+                    if (yön == direction.up)
+                    {
+                        y--;
+                    }
+                    if (yön == direction.down)
+                    {
+                        y++;
+                    }
+
+                    calculatetable();
+                    Thread.Sleep(hız);
+                }
+
+
+
+            })));
+            thread.Start();
+
+        }
+        #endregion
+        #region OYUNBAŞLATMA
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode.ToString() == "B")
+            {
+                if (durdurma==true)
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                   
+                }
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+                label5.Visible = false; // B YE VE D YE BASMAYI GİZLİYORUM
+                label6.Visible = false; // B YE VE D YE BASMAYI GİZLİYORUM
+                panel1.Visible = false;
+                oyunubaslat();
+                
+            }
+
+            
+            if (e.KeyCode.ToString() == "D")
+            {
+                if (durdurma==false)
+                {
+                    this.Focus();
+                    durdurma = true;
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    oyun = false;
+                    
+
+                }
+                else if(durdurma==true)
+                {
+                    timer1.Enabled = true;
+                    timer2.Enabled = true;
+                    oyun = true;
+                    durdurma = false;
+                    oyunubaslat();
+                    
+                }
+
+
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                yön = direction.down;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                yön = direction.up;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                yön = direction.right;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                yön = direction.left;
+            }
+        }
+        #endregion
+        public void herseyisıfırla()
+        {
+           
+            x = 1;
+            y = 1;
+            süre = 0;
+            süre2 = 1;
+            skor = 0;
+            yön = direction.right;
+            kuyruk = new List<Konum>();
+            kuyruk.Add(new Form1.Konum(0, 0));
+            calculatetable();
+            oyunubaslat();
+            
+            
+            oyun = true;
+
+        }
+        #region GEREKSİZLER
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+           
+            
+        }
+
+        private void game_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        #region OYUNSÜRESİ
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            süre++;
+            label3.Text = Convert.ToString(süre);
+        }
+        #endregion
+        #region SKORSÜRESİ
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            süre2++;
+            label4.Text = Convert.ToString(skor);
+
+        }
+        #endregion
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SA_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+            if (kontrol.Text == "kolay")
+            {
+               
+                panel1.Visible = false;
+                button1.Enabled = false;
+                textbox.Enabled = false;
+                this.Focus();
+            }
+            else if (kontrol.Text == "zor")
+            {
+                panel1.Visible = false;
+                button1.Enabled = false;
+                textbox.Enabled = false;
+                this.Focus();
+            }
+            else
+            {
+                MessageBox.Show("LÜTFEN SEVİYE SEÇİNİZ", "HATA");
+            }
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("yilan.txt");  // KAYIT TUTMAK İÇİN YAPIYORUZ
+            sw.Write("KULLANICI:");
+            sw.WriteLine(textbox.Text);
+            sw.Write("SEVİYE:");
+            sw.WriteLine(kontrol.Text);
+            sw.Write("SÜRE:");
+            sw.WriteLine(label3.Text);
+            sw.Write("SKOR:");
+            sw.WriteLine(skor);
+            sw.Close();
+
+            herseyisıfırla();
+            oyun = true;
+            panel2.Visible = false;
+            this.Focus();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            hız = 200;
+            kontrol.Text = "kolay";
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            hız = 80;
+            kontrol.Text = "zor";
+        }
+
+        private void textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textbox_Enter(object sender, EventArgs e)
+        {
+            string isim = textbox.Text;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            StreamReader oku = new StreamReader("yilan.txt");
+            Console.Write(oku);
+        }
+
+
+        #region OYUN
+        public void calculatetable()
+        {
+            if (kuyruk.Count !=1)
+            {
+                for (int i = 1; i < kuyruk.Count; i++)
+                {
+                    if (kuyruk[i].xxx == x && kuyruk[i].yyy == y)
+                    {
+                        Invoke(new Action(() => {
+
+                            oyun = false;
+                            panel2.Visible = true;
+                        }));
+
+                    }
+                }
+            }
+            if (x == yemek.xxx && y == yemek.yyy)
+            {
+                if (yemek.xxx == 1 || yemek.xxx == 500 || yemek.yyy == 1 || yemek.yyy == 500)
+                {
+                    skor = skor + 10;
+                }
+                if (süre2 >=100)
+                {
+                    skor = skor + 0;
+                }
+                else
+                {
+                    skor = skor + 100 / süre2;
+                }
+                
+                süre2 = 1; // 1 eşitledim çünkü art arda doğan yemler ZeroException hatası alıyordum.
+                yemek = new Konum(-1, -1);
+                kuyruk.Add(new Form1.Konum(yemek.xxx, yemek.yyy));
+
+                
+            }
+
+            Random rnd = new Random();
+            Bitmap bitmap = new Bitmap(500, 500);
+            kuyruk[0] = new Konum(x, y);
+            if (x <= 0 || y <= 0 || x == 51 || y == 51)
+            {
+                Invoke(new Action(() => {
+
+                    oyun = false;
+                    panel2.Visible = true;
+
+
+                }));
+              
+              
+                    
+            }
+            else
+            {
+                for (int i = kuyruk.Count - 1; i > 0; i--)
+                {
+                    kuyruk[i] = kuyruk[i - 1];
+                }
+
+                for (int k = 0; k < kuyruk.Count; k++)
+                {
+                    for (int i = (kuyruk[k].xxx - 1) * 10; i < kuyruk[k].xxx * 10; i++)
+                    {
+                        for (int j = (kuyruk[k].yyy - 1) * 10; j < kuyruk[k].yyy * 10; j++)
+                        {
+                            bitmap.SetPixel(i, j, Color.Red);
+                        }
+                    }
+
+                }
+            }
+            if (yemek.xxx == -1)
+            {
+                yemek = new Konum(rnd.Next(1, 50), rnd.Next(1, 50));
+            }
+            for (int i = (yemek.xxx - 1) * 10; i < yemek.xxx * 10; i++)
+            {
+                for (int j = (yemek.yyy - 1) * 10; j < yemek.yyy * 10; j++)
+                {
+                    bitmap.SetPixel(i, j, Color.Blue);
+                }
+
+            }
+            game.Image = bitmap;
+            #endregion
+
+        }
+    }
+}
